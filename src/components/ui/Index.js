64c41b61c1403307446e18b6cc938f0f33f1a9ad
Vishal Vijay Singh <vishalpune2004@gmail.com>
2026-06-21 20:56:56 +0530

    feat(ui): implement index barrel export aggregator for clean library imports

---
+++ src/components/ui/Index.js
@@ -0,0 +1,6 @@
+
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Modal } from './Modal';
+export { default as Loader } from './Loader';
+export { default as Toast } from './Toast';
